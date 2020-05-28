1c6cb967d41159b18e9ce68139a6af19f4c7f565
truehello <trudelmarc@gmail.com>
2020-05-28 15:41:11 -0700

    debug news API

--- functions/news/news.js
+++ functions/news/news.js
@@ -7,35 +7,8 @@ exports.handler = async (event, context) => {
   const country = event.queryStringParameters.country || "us";
   const category = event.queryStringParameters.category || "general";
 
-  console.log("news fetch");
-  //const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
-
-  // exports.handler = async (event, context) => {
-  //   console.log("hello function 2");
-  //   let response;
-  //   try {
-  //     response = await fetch(API_ENDPOINT);
-  //     // handle response
-  //   } catch (err) {
-  //     return {
-  //       statusCode: err.statusCode || 500,
-  //       body: JSON.stringify({
-  //         error: err.message,
-  //       }),
-  //     };
-  //   }
-
-  //   return {
-  //     statusCode: 200,
-  //     //body: `Hello ${response}!`,
-  //      body: JSON.stringify({
-  //        data: response
-  //      })
-  //   };
-  // };
-
-  const url = `${newsapiURL}` + `country=${country}&category=${category}`;
-
+  const url = `${newsapiURL}`+`country=${country}&category=${category}`;
+  console.log(url)
   try {
     const response = await fetch(url, {
       method: "POST",
