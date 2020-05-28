d2e71db0e24e8adabf79e1924f3ed0bb42aec93c
truehello <trudelmarc@gmail.com>
2020-05-28 14:06:43 -0700

    changed client fetch hook

--- src/components/NewsController.js
+++ src/components/NewsController.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LazyLoad from "react-lazyload";
-
-import { useAsyncCombineSeq, useAsyncRun } from "react-hooks-async";
-import { useAsyncTaskDelay } from "react-hooks-async/dist/use-async-task-delay";
-import { useAsyncTaskFetch } from "react-hooks-async/dist/use-async-task-fetch";
+import axios from "axios";
+//import { useAsyncCombineSeq, useAsyncRun } from "react-hooks-async";
+//import { useAsyncTaskDelay } from "react-hooks-async/dist/use-async-task-delay";
+//import { useAsyncTaskFetch } from "react-hooks-async/dist/use-async-task-fetch";
+//import useDataApi from 'use-data-api';
 
 import NewsCard from "./NewsCard";
-import Loader from "./Loader";
+//import Loader from "./Loader";
 
 const Err = ({ error }) => (
   <div>
@@ -14,29 +15,50 @@ const Err = ({ error }) => (
   </div>
 );
 
-
 const envAPIKey = process.env.REACT_APP_NEWS_API_KEY;
 const newsapiURL = process.env.REACT_APP_NEWS_API_URL;
 
-//const newsAPINode = "/.netlify/functions/news"
-
 const NewsController = ({ query, country }) => {
-  const url =
-    `${newsapiURL}` +
-    `country=${country}&category=${query}&` +
-    `apiKey=${envAPIKey}`;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState({ articles: [] });
 
-  const delayTask = useAsyncTaskDelay(500, [query]);
-  const fetchTask = useAsyncTaskFetch(url);
-  const combinedTask = useAsyncCombineSeq(delayTask, fetchTask);
-  useAsyncRun(combinedTask);
-  if (delayTask.pending) return <div className="flex items-center justify-center h-screen">Waiting...</div>;
-  if (fetchTask.error) return <Err error={fetchTask.error} />;
-  if (fetchTask.pending) return <div className="flex items-center justify-center h-screen"><Loader abort={fetchTask.abort} width="200" /></div>
-  if (!fetchTask.result) return <div>No result</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(
+          `${newsapiURL}` +
+            `country=${country}&category=${query}&` +
+            `apiKey=${envAPIKey}`
+        );
+
+        setData(result.data);
+      
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [query, country]);
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        Waiting...
+      </div>
+    );
+  if (isError) return <Err error={isError} />;
+  if (!data) return <div>No result</div>;
+  //console.log(data)
   return (
     <ul className="articleList p-4 lg:px-20 lg:pt-20">
-      {fetchTask.result.articles.map((item) => (
+      {data.articles.map((item) => (
         <LazyLoad height={200} key={item.title} offset={[-200, 0]} once>
           <NewsCard item={item} />
         </LazyLoad>
